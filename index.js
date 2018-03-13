986172d9629f324a59b0aea35f5653464c501f63
Gaurav Rawal <gaurav.rawal66@gmail.com>
2018-03-13 09:41:46 +0530

    Changes Added

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
-module.exports = (robot) => {
-  // Your code here
-  robot.log('Yay, the app was loaded!')
-
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
-}
+module.exports = robot => {
+  robot.on('issues.opened', async context => {
+    const params = context.issue({
+      body:
+        'Hi!, Thanks for pointing out, our maintainers will check out this issue soon. '
+    });
+    return context.github.issues.createComment(params);
+  });
+  robot.on('issues.labeled', async context => {
+    const params = context.issue({ body: 'Thanks for adding the label' });
+    return context.github.issues.createComment(params);
+  });
+};
